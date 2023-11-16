3303dfd8191aed30b8a156d96137092801bfb891
aravindhan.s <aravindhan.s@trdfin.in>
2023-11-16 15:23:45 +0530

    ara

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const cookieParser = require("cookie-parser");
 var morgan = require("morgan");
 const CardRouter = require("./routers/Cardrouter");
 const path = require("path");
-// const ejs = require("ejs");
-const exportrouter = require("./routers/export");
+const ejs = require("ejs");
 
 //middleware
 
@@ -49,11 +48,10 @@ app.get("/", (req, res) => {
 app.use("/api/", forgetpassword);
 app.use("/api/", resetpassword);
 app.use("/api/auth/", authrouter);
-// app.use("/api/profile/", profilerouter);
+app.use("/api/profile/", profilerouter);
 app.use("/api/user/", userRouter);
 app.use("/api/company/", companyrouter);
 app.use("/api/card/", CardRouter);
-app.use("/api/", exportrouter);
 
 app.use((err, req, res, next) => {
   const errorstatus = err.status || 500;
@@ -66,9 +64,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-console.log("hi this aravindhan");
 const port = process.env.PORT;
 app.listen(port, async () => {
   await mongodb();
-  console.log("Port is Running 4001");
+  console.log("Port is Running 4000");
 });
